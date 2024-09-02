956742890b5b796ffe50d5f4e6f47c556a34654b
CyriacduChatenet <cyriacduchatenet@icloud.com>
2024-09-02 11:47:24 +0200

    fix(client): :ambulance: api url env var

--- client/src/app/utils/axios.utils.ts
+++ client/src/app/utils/axios.utils.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const API = () => {
     return axios.create({
-        baseURL: 'http://localhost:8000/api/v1',
+        baseURL: import.meta.env.VITE_APP_API_URL,
         timeout: 1000,
       })
 }
