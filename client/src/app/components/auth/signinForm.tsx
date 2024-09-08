973b0bc62e6b3c7a7020dbd312d9e1cfd0952c2d
CyriacduChatenet <cyriacduchatenet@icloud.com>
2024-09-08 19:06:02 +0200

    feat(client): :sparkles: add signin form

--- client/src/app/components/auth/signinForm.tsx
+++ client/src/app/components/auth/signinForm.tsx
@@ -9,9 +9,9 @@ import { Role } from "../../enums/role.enum";
 import useAuth from "../../context/auth.context";
 
 export const SigninForm: FC = () => {
-    const authService = new AuthService();
-    const { setUser } = useUser();
-    const { setAuth } = useAuth();
+  const authService = new AuthService();
+  const { setUser } = useUser();
+  const { setAuth } = useAuth();
   const {
     register,
     handleSubmit,
@@ -19,11 +19,10 @@ export const SigninForm: FC = () => {
   } = useForm();
   const navigate = useNavigate();
 
-
   const onSubmit = async (data) => {
     const isAuth = await authService.signin(data);
 
-    if(isAuth) {
+    if (isAuth) {
       setAuth(isAuth);
       const encodedToken = window.localStorage.getItem("access_token");
 
@@ -33,36 +32,84 @@ export const SigninForm: FC = () => {
           id: string;
           roles: string;
         } = jwtDecode(encodedToken);
-        
+
         setUser({
           email: decodedToken.email,
           id: decodedToken.id,
           roles: decodedToken.roles,
         });
-  
-        if(decodedToken.roles === Role.Admin) {
-          navigate('/admin/dashboard');
-      } else {
-          navigate('/dashboard');
+
+        if (decodedToken.roles === Role.Admin) {
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/dashboard");
+        }
       }
-    }
     }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex-col items-center justify-center">
-        <label htmlFor="email">
-            <p>Email</p>
-            <input type="email" {...register("email", { required: true })} autoComplete="email" />
-            {errors.email && <span className="text-red-500">This field is required</span>}
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Email address
         </label>
-        <label htmlFor="password">
-            <p>Password</p>
-            <input type="password" {...register("password", { required: true })} autoComplete="password" />
-            {errors.email && <span className="text-red-500">This field is required</span>}
-        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            type="email"
+            {...register("email", { required: true })}
+            autoComplete="email"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          {errors.email && (
+            <span className="text-red-500">This field is required</span>
+          )}
+        </div>
+      </div>
 
-      <input type="submit" className="bg-blue-600 text-white" />
+      <div>
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Password
+          </label>
+          <div className="text-sm">
+            <a
+              href="#"
+              className="font-semibold text-indigo-600 hover:text-indigo-500"
+            >
+              Forgot password?
+            </a>
+          </div>
+        </div>
+        <div className="mt-2">
+          <input
+            id="password"
+            type="password"
+            {...register("password", { required: true })}
+            autoComplete="current-password"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          {errors.password && (
+            <span className="text-red-500">This field is required</span>
+          )}
+        </div>
+      </div>
+
+      <div>
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Sign in
+        </button>
+      </div>
     </form>
   );
 };
--- client/src/app/pages/auth/signin.tsx
+++ client/src/app/pages/auth/signin.tsx
@@ -1,14 +1,38 @@
 import { FC } from "react";
 
 import { SigninForm } from "../../components/auth/signinForm";
+import ShopLayout from "../../components/shop/layout/layout";
 
 const SigninPage: FC = () => {
-    return(
-        <div>
-            <h1>Signin</h1>
-            <SigninForm/>
+  return (
+    <ShopLayout>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 my-40">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            alt="Your Company"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto h-10 w-auto"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
         </div>
-    )
-}
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <SigninForm />
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{" "}
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Sign up
+            </a>
+          </p>
+        </div>
+      </div>
+    </ShopLayout>
+  );
+};
 
 export default SigninPage;
