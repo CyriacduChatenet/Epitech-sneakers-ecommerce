35351e387df981b3e83d74931cc3a2d4f51614f3
CyriacduChatenet <cyriacduchatenet@icloud.com>
2024-09-08 19:04:16 +0200

    feat(client): :sparkles: update navigation

--- client/src/app/components/shop/navigation/navigation.tsx
+++ client/src/app/components/shop/navigation/navigation.tsx
@@ -3,6 +3,8 @@ import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link, useLocation } from "react-router-dom";
 
+import useUser from "../../../context/user.context";
+
 export const Navigation: FC = () => {
   const navigation = [
     { name: "Men", href: "/shop/men" },
@@ -12,6 +14,7 @@ export const Navigation: FC = () => {
 
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const location = useLocation();
+  const { user } = useUser();
   const pathname = location.pathname;
 
   return (
@@ -56,12 +59,31 @@ export const Navigation: FC = () => {
           ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <Link
-            to="/signin"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            Sign in <span aria-hidden="true">&rarr;</span>
-          </Link>
+          {user.email.length === 0 ? (
+            <Link
+              to="/signin"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Sign in <span aria-hidden="true">&rarr;</span>
+            </Link>
+          ) : (
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                />
+              </svg>
+            </button>
+          )}
         </div>
       </nav>
       <Dialog
@@ -103,12 +125,31 @@ export const Navigation: FC = () => {
                 ))}
               </div>
               <div className="py-6">
-                <Link
-                  to="/signin"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Sign in
-                </Link>
+                {user.email.length === 0 ? (
+                  <Link
+                    to="/signin"
+                    className="text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Sign in <span aria-hidden="true">&rarr;</span>
+                  </Link>
+                ) : (
+                  <button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                      />
+                    </svg>
+                  </button>
+                )}
               </div>
             </div>
           </div>
