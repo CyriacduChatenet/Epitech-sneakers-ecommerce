045bca89e413eb0cfde6ca8228cafc3a0d29f964
CyriacduChatenet <cyriacduchatenet@icloud.com>
2024-09-10 12:09:11 +0200

    fix(client): :lipstick: change shop title for gender

--- client/src/app/components/shop/product-list/product-list.tsx
+++ client/src/app/components/shop/product-list/product-list.tsx
@@ -10,11 +10,16 @@ interface IProps {
 }
 
 export const ProductList: FC<IProps> = ({ products, total, gender }) => {
+
+  const capitalizeFirstLetter = (sentence: string) => {
+    return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+  }
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-        {gender}'s Shoes & Sneakers ({total})
+          {capitalizeFirstLetter(`${gender}'s Shoes & Sneakers ( ${total} )`)}
         </h2>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
@@ -22,7 +27,7 @@ export const ProductList: FC<IProps> = ({ products, total, gender }) => {
             <ProductCard key={product.id} data={product} />
           ))}
 
-          {products.length === 0 && (<div className="min-h-96"><p>No products found</p></div>)}
+          {products.length === 0 && (<div className="min-h-"><p>No products found</p></div>)}
         </div>
       </div>
     </div>
