aeb18ae5b91b9a40672b5a3ce5fc166cfe8bb708
CyriacduChatenet <cyriacduchatenet@icloud.com>
2024-09-10 15:40:23 +0200

    fix(client): :fire: fix https config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mkcert from'vite-plugin-mkcert'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    https: true
+    https: process.env.NODE_ENV === "development" ? true : false,
   },
   plugins: [react(), mkcert()],
-})
+});
