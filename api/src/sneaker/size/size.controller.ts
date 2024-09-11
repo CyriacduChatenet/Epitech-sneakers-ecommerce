e422a6775d8b85b8fc3268a59f359a01b6654aef
CyriacduChatenet <cyriacduchatenet@icloud.com>
2024-09-11 17:50:51 +0200

    fix(api): :lock: add guards

--- api/src/sneaker/size/size.controller.ts
+++ api/src/sneaker/size/size.controller.ts
@@ -7,18 +7,24 @@ import {
   Param,
   Delete,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 
 import { SizeService } from './size.service';
 import { CreateSizeDto } from './dto/create-size.dto';
 import { UpdateSizeDto } from './dto/update-size.dto';
 import { ApiQuery } from '../../types/api.type';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { Role } from '../../enums/role.enum';
+import { Roles } from '../../decorators/role.decorator';
 
 @Controller('size')
 export class SizeController {
   constructor(private readonly sizeService: SizeService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.Admin)
   create(@Body() createSizeDto: CreateSizeDto) {
     return this.sizeService.create(createSizeDto);
   }
@@ -34,11 +40,15 @@ export class SizeController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.Admin)
   update(@Param('id') id: string, @Body() updateSizeDto: UpdateSizeDto) {
     return this.sizeService.update(id, updateSizeDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.Admin)
   remove(@Param('id') id: string) {
     return this.sizeService.remove(id);
   }
--- api/src/stock/stock.controller.ts
+++ api/src/stock/stock.controller.ts
@@ -7,18 +7,24 @@ import {
   Param,
   Delete,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 
 import { StockService } from './stock.service';
 import { CreateStockDto } from './dto/create-stock.dto';
 import { UpdateStockDto } from './dto/update-stock.dto';
-import { ApiQuery } from '..//types/api.type';
+import { ApiQuery } from '../types/api.type';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Roles } from '../decorators/role.decorator';
+import { Role } from '../enums/role.enum';
 
 @Controller('stock')
 export class StockController {
   constructor(private readonly stockService: StockService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.Admin)
   create(@Body() createStockDto: CreateStockDto) {
     return this.stockService.create(createStockDto);
   }
@@ -34,11 +40,15 @@ export class StockController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.Admin)
   update(@Param('id') id: string, @Body() updateStockDto: UpdateStockDto) {
     return this.stockService.update(id, updateStockDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.Admin)
   remove(@Param('id') id: string) {
     return this.stockService.remove(id);
   }
